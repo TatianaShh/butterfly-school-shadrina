39498c064b645a2b98ebcb53d0b433275d7d34fa
TatianaShh <tatiana.shh@mail.ru>
2022-02-16 13:02:48 +0300

    не работает сортировка каталога :(

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,64 @@
-console.log('script work!');
+$(document).ready(function(){
+
+// табы в контактах
+
+  $('.j-tabs-link').on('click', function(event){
+    event.preventDefault();
+
+    const index = $(this).index('.j-tabs-link');
+
+    $('.j-tabs-link').removeClass('active');
+    $(this).addClass('active');
+
+    $('.j-tabs-content').removeClass('active');
+    $('.j-tabs-content').eq(index).addClass('active');
+  });
+
+// аккордеон
+
+  let prevBtn;
+
+  $('.j-accordion-btn').on('click', function(){
+  if (this === prevBtn) {
+    $(this).next().slideToggle();
+    return;
+  }
+
+    $('.j-accordion-btn').next().slideUp();
+    $(this).next().slideToggle();
+    prevBtn = this;
+  });
+
+// слайдер
+
+  $('.j-slider').slick({
+  });
+
+// фильтр бабочек
+
+ $('.j-filter-link').on('click', function(event){
+  event.preventDefault();
+
+  const filterType = $(this).data('filter');
+
+  $('.j-filter-link').removeClass('active');
+  $(this).addClass('active');
+
+  if(filterType === 'all') {
+    $('.j-case').show();
+    return;
+  }
+
+  $('.j-case').each(function() {
+    const caseType = $(this).data('type');
+
+    if (filterType === caseType) {
+      $(this).show();
+      return;
+    }
+
+    $(this).hide();
+  });
+});
+
+});
